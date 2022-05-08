a38561f78b0b6401bcb9ab7fb87bffcd7b7f85cf
Ayhan <ayhancoemert@gmail.com>
2022-05-08 22:36:03 +0200

    Änderungen vorgenommen

--- src/server/db/AktivitätMapper.py
+++ src/server/db/AktivitätMapper.py
@@ -1,8 +1,8 @@
-from server.bo.Aktivität import Aktivität
+from server.bo.Aktivitaet import Aktivitaet
 from server.db.Mapper import Mapper
 
 
-class AktivitätMapper(Mapper):
+class AktivitaetMapper(Mapper):
     """Mapper-Klasse, die Konversation-Objekte auf eine relationale
     Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfügung
     gestellt, mit deren Hilfe z.B. Objekte gesucht, erzeugt, modifiziert und
@@ -13,22 +13,21 @@ class AktivitätMapper(Mapper):
         super().__init__()
 
     def find_all(self):
-        """Auslesen aller Aktivität.
-        :return Eine Sammlung mit Aktivität-Objekten, die sämtliche Aktivität repräsentieren.
+        """Auslesen aller Aktivitaet.
+        :return Eine Sammlung mit Aktivitaet-Objekten, die sämtliche Aktivitaet repräsentieren.
         """
         result = []
         cursor = self._cnx.cursor()
-        cursor.execute("SELECT * from Aktivität")
+        cursor.execute("SELECT * from aktivitaet")
         tuples = cursor.fetchall()
 
-        for (id,creation_time, aktivität_id, bezeichnung, kapazität_in_personentagen,) in tuples:
-            aktivität= Aktivität()
-            aktivität.set_id(id)
-            aktivität.set_creation_time(creation_time)
-            aktivität.set_aktivität_id(aktivität_id)
-            aktivität.set_bezeichnung(bezeichnung)
-            aktivität.set_kapazität_in_personentagen(kapazität_in_personentagen)
-            result.append(aktivität)
+        for (id,creation_time,bezeichnung, kapazitaet_in_personentagen,) in tuples:
+            aktivitaet= Aktivitaet()
+            aktivitaet.set_id(id)
+            aktivitaet.set_creation_time(creation_time)
+            aktivitaet.set_bezeichnung(bezeichnung)
+            aktivitaet.set_kapazitaet_in_personentagen(kapazitaet_in_personentagen)
+            result.append(aktivitaet)
 
         self._cnx.commit()
         cursor.close()
@@ -36,10 +35,10 @@ class AktivitätMapper(Mapper):
         return result
 
     def find_by_key(self, key):
-        """Auslesen aller Chats anhand der ID,
+        """Auslesen aller Aktivitaet anhand der ID,
         da diese vorgegeben ist, wird genau ein Objekt zurückgegeben.
         :param key Primärschlüsselattribut
-        :return Aktivität-Objekt, das dem übergebenen Schlüssel entspricht, None bei
+        :return Aktivitaet-Objekt, das dem übergebenen Schlüssel entspricht, None bei
         nicht vorhandenem DB-Tupel
         """
         result = None
@@ -48,28 +47,28 @@ class AktivitätMapper(Mapper):
         command = "SELECT * FROM aktivität WHERE id={}".format(key)
         cursor.execute(command)
         tuples = cursor.fetchall()
-        for (id, creation_time, aktivität_id, bezeichnung, kapazität_in_personentagen,) in tuples:
-            aktivtät = Aktivität()
-            aktivität.set_id(id)
-            aktivität.set_creation_time(creation_time)
-            aktivität.set_bezeichnung(bezeichnung_id)
-            aktivität.set_kapazität_in_personentagen(kapazität_in_personentagen)
+        for (id, creation_time, bezeichnung, kapazitaet_in_personentagen) in tuples:
+            aktivitaet = Aktivitaet()
+            aktivitaet.set_id(id)
+            aktivitaet.set_creation_time(creation_time)
+            aktivitaet.set_bezeichnung(bezeichnung)
+            aktivitaet.set_kapazitaet_in_personentagen(kapazitaet_in_personentagen)
 
-        result = aktivität
+        result = aktivitaet
 
         self._cnx.commit()
         cursor.close()
         return result
 
-    def insert(self, aktivität):
-        """Einfügen eines Aktivität-Objekts in die Datenbank.
+    def insert(self, aktivitaet):
+        """Einfügen eines Aktivitaet-Objekts in die Datenbank.
         Dabei wird auch der Primärschlüssel des übergebenen Objekts geprüft und ggf.
         berichtigt.
-        :param aktivität das zu speichernde Objekt
+        :param aktivitaet das zu speichernde Objekt
         :return das bereits übergebene Objekt, jedoch mit ggf. korrigierter ID.
         """
         cursor = self._cnx.cursor()
-        cursor.execute("SELECT MAX(id) AS maxid FROM aktivität ")
+        cursor.execute("SELECT MAX(id) AS maxid FROM aktivitaet ")
         tuples = cursor.fetchall()
 
         for (maxid) in tuples:
@@ -80,11 +79,11 @@ class AktivitätMapper(Mapper):
             else:
                 """Wenn wir keine maximale ID feststellen konnten, dann gehen wir
                 davon aus, dass die Tabelle leer ist und wir mit der ID 1 beginnen können."""
-                aktivität.set_id(1)
+                aktivitaet.set_id(1)
 
-        command = "INSERT INTO aktivität (id, creation_time, bezeichnung, kapazität_in_personentagen,) VALUES (%s,%s,%s,%s,%s,%s)"
+        command = "INSERT INTO aktivitaet (id, creation_time, bezeichnung, kapazitaet_in_personentagen,) VALUES (%s,%s,%s,%s,%s,%s)"
         data = (
-        aktivität.get_id(), aktivität.get_creation_time(), aktivität.get_bezeichnung(), chat.get_kapazität_in_personentagen())
+        aktivitaet.get_id(), aktivitaet.get_creation_time(), aktivitaet.get_bezeichnung(), aktivitaet.get_kapazitaet_in_personentagen())
         cursor.execute(command, data)
 
         self._cnx.commit()
@@ -92,50 +91,52 @@ class AktivitätMapper(Mapper):
 
         return aktivität
 
-    def update(self, aktivität):
+    def update(self, aktivitaet):
         """Wiederholtes Schreiben eines Objekts in die Datenbank.
-        :param aktivität das Objekt, das in die DB geschrieben werden soll
+        :param aktivitaet das Objekt, das in die DB geschrieben werden soll
         """
         cursor = self._cnx.cursor()
 
-        command = "UPDATE aktivität SET bezeichnung=%s,kapazität_in_personentagen=%s,WHERE id=%s"
-        data = (chat.get_bezeichnung(), chat.get_kapazität_in_personentagen(),chat.get_id())
+        command = "UPDATE aktivitaet SET bezeichnung=%s,kapazitaet_in_personentagen=%s,WHERE id=%s"
+        data = (aktivitaet.get_bezeichnung(), aktivitaet.get_kapazitaet_in_personentagen(),aktivitaeti.get_id())
 
         cursor.execute(command, data)
 
         self._cnx.commit()
         cursor.close()
 
-    def delete(self, aktivität):
+    def delete(self, aktivitaet):
         """Löschen der Daten eines Projekt-Objekts aus der Datenbank.
-        :param aktivität das aus der DB zu löschende "Objekt"
+        :param aktivitaet das aus der DB zu löschende "Objekt"
         """
         cursor = self._cnx.cursor()
 
-        command = "DELETE FROM aktivität WHERE id={}".format(aktivität.get_id())
+        command = "DELETE FROM aktivitaet WHERE id={}".format(aktivitaet.get_id())
         cursor.execute(command)
 
         self._cnx.commit()
         cursor.close()
 
-        return aktivität
+        return aktivitaet
 
-    def find_by_all_by_aktivität_id(self, aktivität_id):
+
+
+    def find_by_bezeichnung(self, bezeichnung):
+
         result = []
-
         cursor = self._cnx.cursor()
-        command = " SELECT id, creation_time, bezeic, is_accepted,sender, message FROM chat WHERE learngroup_id ={} ORDER BY id".format(
-            aktivität_id)
+        command = "SELECT id, creation_time, bezeichnung, kapazitaet_in_personentagen FROM aktivitaet WHERE bezeichnung={}".format(bezeichnung)
+
         cursor.execute(command)
         tuples = cursor.fetchall()
 
-        for (id, creation_time, aktivität_id, bezeichnung, kapazität_in_personentagen) in tuples:
-            aktivität = Aktivität()
-            aktivität.set_id(id)
-            aktivität.set_creation_time(creation_time)
-            aktivität.set_bezeichnung(bezeichnung)
-            aktivität.set_kapazität_in_personentagen(kapazität_in_personentagen)
-            result.append(aktivität)
+        for (id, creation_time, bezeichnung, kapazitaet_in_personentagen) in tuples:
+            aktivitaet = Aktivitaet()
+            aktivitaet.set_id(id)
+            aktivitaet.set_creation_time(creation_time)
+            aktivitaet.set_bezeichnung(bezeichnung)
+            aktivitaet.set_kapazitaet_in_personentagen(kapazitaet_in_personentagen)
+            result.append(aktivitaet)
 
         self._cnx.commit()
         cursor.close()
@@ -144,9 +145,9 @@ class AktivitätMapper(Mapper):
 
     # Zum Testen ausführen
     if (__name__ == "__main__"):
-        with AktivitätMapper() as mapper:
-            aktivität = aktivität()
-            aktivität.set_name("Mathe Chat")
-            aktivität.set_id(2)
+        with AktivitaetMapper() as mapper:
+            aktivitaet = aktivität()
+            aktivitaet.set_name("Mathe Chat")
+            aktivitaet.set_id(2)
 
-            mapper.insert(aktivität)
+            mapper.insert(aktivitaet)
