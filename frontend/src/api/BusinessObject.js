c026db03144259d7033f312685229f7290bc09f9
Ayhan <ayhancoemert@gmail.com>
2022-06-15 00:36:01 +0200

    Korrektur BO vorgenommen

--- frontend/src/api/BusinessObject.js
+++ frontend/src/api/BusinessObject.js
@@ -1,35 +1,39 @@
-export default class BusinessObject {
+/**
+ * Base class for all BusinessObjects, which has an ID field by default.
+ */
+ export default class BusinessObject {
 
+    /**
+     * The null constructor.
+     */
     constructor() {
       this.id = 0;
-      this.creation_date = 0;
-    }
-  
-    // Sets the id of the BusinessObject */
-    setID(id) {
-      this.id = id;
     }
 
-    /** gets the id of the BusinessObject */
+    /**
+     * Sets the ID of this BusinessObject.
+     *
+     * @param {*} aId - the new ID of this BusinessObject
+     */
+    setID(aId) {
+      this.id = aId;
+    }
+
+    /**
+     * Returns the ID of this BusinessObject.
+     */
     getID() {
       return this.id;
     }
 
-    // Sets the creation_date of the BusinessObject */
-    setCreation_date(creation_date) {
-      this.creation_date = creation_date
+    /**
+     * Returns a string representation of this Object. This is useful for debugging purposes.
+     */
+    toString() {
+      let result = '';
+      for (var prop in this) {
+        result += prop + ': ' + this[prop] + ' ';
+      }
+      return result;
     }
-    /** gets the creation_date of the BusinessObject */
-    getCreation_date() {
-      return this.creation_date
-    }
-  
-  toString() {
-    let result = "";
-    for (var prop in this) {
-      result += prop + ": " + this[prop] + " ";
-    }
-
-    return result;
   }
-}
