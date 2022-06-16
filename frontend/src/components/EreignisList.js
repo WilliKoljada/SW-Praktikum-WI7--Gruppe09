5d7a0fc32935e8feab7decfe29b85bee2f972f9c
WilliKoljada <78151682+WilliKoljada@users.noreply.github.com>
2022-06-16 19:02:34 +0200

    Ereignisse aus dem Backend gefetcht.

--- frontend/src/components/EreignisList.js
+++ frontend/src/components/EreignisList.js
@@ -25,7 +25,6 @@ class EreignisList extends Component {
     // Init the state
     this.state = {
       ereigniss: [],
-      filteredEreigniss: [],
       ereignisFilter: "",
       error: null,
       loadingInProgress: false,
@@ -34,6 +33,103 @@ class EreignisList extends Component {
     };
   }
 
+  /** Fetches EreignisBOs for the current person */
+  getEreigniss = () => {
+    ZeiterfassungAPI.getAPI().getEreigniss().then(ereignisBOs =>
+      this.setState({  // Set new state when EreignisBOs have been fetched
+        ereigniss: ereignisBOs,
+        loadingInProgress: false, // loading indicator
+        loadingEreignisError: null
+      })).catch(e =>
+        this.setState({ // Reset state with error from catch
+          ereigniss: [],
+          loadingInProgress: false,
+          loadingEreignisError: e
+
+      )
+    );
+
+    // set loading to true
+    this.setState({
+      loadingInProgress: true,
+      loadingEreignisError: null
+    });
+  }
+
+  /** Lifecycle method, which is called when the component gets inserted into the browsers DOM */
+  componentDidMount() {
+    this.getEreigniss();
+  }
+
+  /**
+   * Handles onExpandedStateChange events from the EreignisListEntry component. Toggels the expanded state of
+   * the EreignisListEntry of the given EreignisBO.
+   *
+   * @param {ereignis} EreignisBO of the EreignisListEntry to be toggeled
+   */
+   onExpandedStateChange = ereignis => {
+    // console.log(ereignisID);
+    // Set expandend ereignis entry to null by default
+    let newID = null;
+
+    // If same ereignis entry is clicked, collapse it else expand a new one
+    if (ereignis.getID() !== this.state.expandedEreignisID) {
+      // Expand the ereignis entry with ereignisID
+      newID = ereignis.getID();
+    }
+    // console.log(newID);
+    this.setState({
+      expandedEreignisID: newID,
+    });
+  }
+
+  /**
+   * Handles onEreignisDeleted events from the EreignisListEntry component
+   *
+   * @param {ereignis} EreignisBO of the EreignisListEntry to be deleted
+   */
+
+  /** Handles the onClick event of the add ereignis button */
+  addEreignisButtonClicked = event => {
+    // Do not toggle the expanded state
+    event.stopPropagation();
+    //Show the EreignisForm
+    this.setState({
+      showEreignisForm: true
+    });
+  }
+
+  /** Handles the onClose event of the EreignisForm */
+  ereignisFormClosed = ereignis => {
+    // ereignis is not null and therefore created
+    if(ereignis){
+      const newEreignisList = [...this.state.ereigniss, ereignis];
+      this.setState({
+        ereigniss: newEreignisList,
+        filteredEreigniss: [...newEreignisList],
+        showEreignisForm: false
+      });
+    } else {
+      this.setState({
+        showEreignisForm: false
+      });
+    }
+  }
+
+  /** Handels onChange events of the ereignis filter text field */
+  filterFieldValueChange = event =>
+
+  /** Handles the onClose event of the clear filter button */
+  clearFilterFieldButtonClicked = () => {
+    // Reset the filter
+    this.setState({
+      filteredEreigniss: [...this.state.ereigniss],
+      ereignisFilter: ""
+    });
+  }
+
+  /** Renders the component */
+  render()
 }
 
 /** Component specific styles */
