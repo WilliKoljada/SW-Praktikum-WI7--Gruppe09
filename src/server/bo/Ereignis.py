db0c4f06b310fe5c25ac2b74fd9d6a12514bee30
WilliKoljada <78151682+WilliKoljada@users.noreply.github.com>
2022-06-16 07:20:05 +0200

    Ereignis.py erweitert

--- src/server/bo/Ereignis.py
+++ src/server/bo/Ereignis.py
@@ -1,28 +1,44 @@
 from server.bo import BusinessObject as bo
-
+from datetime import datetime
 
 
 class Ereignis (bo.BusinessObject):
 
     def __init__(self):
         super().__init__()
-        self._zeitpunkt_ereigniseintritt = ""
-
-    def get_zeitpunkt_ereigniseintritt(self):
-        return self._zeitpunkt_ereigniseintritt
-
-    def set_zeitpunkt_ereigniseintritt(self, value):
-        self._zeitpunkt_ereigniseintritt = value
+        self._type = ""
+        self._datum = None
+        self._startzeit = None
 
 
+    def get_type(self):
+        return self._type
+
+    def set_type(self, value):
+        self._type = value
+
+    def get_datum(self):
+        return self._datum
+
+    def set_datum(self, datum):
+        self._datum = datum
+
+    def get_startzeit(self):
+        return self._startzeit
 
 
+
+    def get_personID(self):
+        return self._personID
+
+    def set_personID(self, personID):
+        self._personID = personID
 
     # User Object aus Dict erstellen
     @staticmethod
     def from_dict(dictionary=dict()):
         obj = Ereignis()
         obj.set_id(dictionary["id"])
-        obj.set_zeitpunkt_ereigniseintritt(dictionary["zeitpunkt_ereigniseintritt"])
+
 
         return obj
