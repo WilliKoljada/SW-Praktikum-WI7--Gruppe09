510df29ddbebc30306dd5055444ab4498a2c06d4
WilliKoljada <78151682+WilliKoljada@users.noreply.github.com>
2022-06-17 14:21:56 +0200

    Themeanpassungen

--- frontend/src/components/layout/Theme.js
+++ frontend/src/components/layout/Theme.js
@@ -1,11 +1,11 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import { colors } from '@material-ui/core';
 
 const white = '#FFFFFF';
 const black = '#000000';
 
 // A custom theme for this app
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     black,
     white,
