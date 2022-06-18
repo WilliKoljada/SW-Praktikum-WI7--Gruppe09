d0e83e9113efba3e1454c6c2abbee6e6299d56e0
Ayhan <ayhancoemert@gmail.com>
2022-06-18 16:05:57 +0200

    Ergänzungen vorgenommen

--- frontend/src/components/entries/AktivitaetListEntry.js
+++ frontend/src/components/entries/AktivitaetListEntry.js
@@ -17,3 +17,148 @@ import AktivitaetList from "../AktivitaetList";
  * @author
  */
 class AktivitaetListEntry extends Component {
+
+constructor(props) {
+    super(props);
+
+    // Init the state
+    this.state = {
+      aktivitaet: props.aktivitaet,
+      showAktivitaetForm: false,
+      showAktivitaetDeleteDialog: false,
+    };
+  }
+
+  /** Handles onChange events of the underlying ExpansionPanel */
+  expansionPanelStateChanged = () => {
+    this.props.onExpandedStateChange(this.props.aktivitaet);
+  }
+
+  /** Handles onAktivitaetDelete events from an AktivitaetListEntry  */
+  deleteAktivitaetHandler = (deletedAktivitaet) => {
+    // console.log(deletedAktivitaet.getID());
+    this.setState({
+      aktivitaets: this.state.aktivitaets.filter(aktivitaet => aktivitaet.getID() !== deletedAktivitaet.getID())
+    })
+  }
+
+  /** Handles the onClick event of the edit aktivitaet button */
+  editAktivitaetButtonClicked = (event) => {
+    event.stopPropagation();
+    this.setState({
+      showAktivitaetForm: true
+    });
+  }
+
+  /** Handles the onClose event of the AktivitaetForm */
+  aktivitaetFormClosed = (aktivitaet) => {
+    // aktivitaet is not null and therefor changed
+    if (aktivitaet) {
+      this.setState({
+        aktivitaet: aktivitaet,
+        showAktivitaetForm: false
+      });
+    } else {
+      this.setState({
+        showAktivitaetForm: false
+      });
+    }
+  }
+
+  /** Handles the onClick event of the delete aktivitaet button */
+  deleteAktivitaetButtonClicked = (event) => {
+    event.stopPropagation();
+    this.setState({
+      showAktivitaetDeleteDialog: true
+    });
+  }
+
+  /** Handles the onClose event of the AktivitaetDeleteDialog */
+  deleteAktivitaetDialogClosed = (aktivitaet) => {
+    // if aktivitaet is not null, delete it
+    if (aktivitaet) {
+      this.props.onAktivitaetDeleted(aktivitaet);
+    };
+
+    // Don´t show the dialog
+    this.setState({
+      showAktivitaetDeleteDialog: false
+    });
+  }
+
+  /** Renders the component */
+  render() {
+    const { classes, expandedState } = this.props;
+    // Use the states aktivitaet
+    const { aktivitaet, showAktivitaetForm, showAktivitaetDeleteDialog } = this.state;
+
+    // console.log(this.state);
+    return (
+      <div>
+        <Accordion defaultExpanded={false} expanded={expandedState} onChange={this.expansionPanelStateChanged}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id={`aktivitaet${aktivitaet.getID()}aktivitaetpanel-header`}
+          >
+            <Grid container spacing={1} justifyContent="flex-start" alignItems="center">
+              <Grid item>
+                <Typography variant="body1" className={classes.heading}>
+                    {aktivitaet.getName()}, {aktivitaet.getBezeichnung()}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <ButtonGroup variant="text" size="small">
+                  <Button color="primary" onClick={this.editAktivitaetButtonClicked}>
+                    edit
+                  </Button>
+                  <Button color="secondary" onClick={this.deleteAktivitaetButtonClicked}>
+                    delete
+                  </Button>
+                </ButtonGroup>
+              </Grid>
+              <Grid item xs />
+              <Grid item>
+                <Typography variant="body2" color={"textSecondary"}>List of aktivitäten</Typography>
+              </Grid>
+            </Grid>
+          </AccordionSummary>
+          <AccordionDetails>
+            <AktivitaetList show={expandedState} aktivitaet={aktivitaet} />
+          </AccordionDetails>
+        </Accordion>
+        <AktivitaetForm show={showAktivitaetForm} aktivitaet={aktivitaet} onClose={this.aktivitaetFormClosed} />
+        <AktivitaetDeleteDialog show={showAktivitaetDeleteDialog} aktivitaet={aktivitaet} onClose={this.deleteAktivitaetDialogClosed} />
+      </div>
+    );
+  }
+}
+
+/** Component specific styles */
+const styles = theme => ({
+  root: {
+    width: "100%",
+  }
+});
+
+/** PropTypes */
+AktivitaetListEntry.propTypes = {
+  /** @ignore */
+  classes: PropTypes.object.isRequired,
+  /** The AktivitaetBO to be rendered */
+  aktivitaet: PropTypes.object.isRequired,
+  /** The state of this AktivitaetListEntry. If true the aktivitaet is shown with its accounts */
+  expandedState: PropTypes.bool.isRequired,
+  /** The handler responsible for handle expanded state changes (exanding/collapsing) of this AktivitaetListEntry
+   *
+   * Signature: onExpandedStateChange(AktivitaetBO aktivitaet)
+   */
+  onExpandedStateChange: PropTypes.func.isRequired,
+  /**
+   *  Event Handler function which is called after a sucessfull delete of this aktivitaet.
+   *
+   * Signature: onAktivitaetDelete(AktivitaetBO aktivitaet)
+   */
+  onAktivitaetDeleted: PropTypes.func.isRequired
+}
+
+export default withStyles(styles)(AktivitaetListEntry);
