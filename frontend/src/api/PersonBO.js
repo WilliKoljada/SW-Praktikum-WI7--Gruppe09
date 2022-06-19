521d0f920db1eea6f18c00b5775babde02d90ed6
Aniqa301 <103525488+Aniqa301@users.noreply.github.com>
2022-06-19 13:21:51 +0200

    richtig eingerückt

--- frontend/src/api/PersonBO.js
+++ frontend/src/api/PersonBO.js
@@ -1,73 +1,62 @@
-import BusinessObject from "./BusinessObject";
+import BusinessObject from './BusinessObject';
 
 
 export default class PersonBO extends BusinessObject {
-
     constructor(google_id, vorname, nachname, email, benutzername){
         super();
         this.google_id = google_id;
         this.vorname = vorname;
         this.nachname = nachname;
         this.email = email;
-        this.benutzername = benutzername
-        this.role = role
+        this.benutzername = benutzername;
     }
     // Sets the google_id of the PersonBO */
     setGoogle_id(google_id) {
-    this.google_id = google_id;
+      this.google_id = google_id;
     }
     /** gets the google_id of the PersonBO */
     getGoogle_id() {
-    return this.google_id;
+     return this.google_id;
     }
     // Sets the vorname of the PersonBO */
     setVorname(vorname) {
-    this.vorname = vorname;
-    }  
+      this.vorname = vorname;
+    }
     /** gets the vorname of the PersonBO */
     getVorname() {
-    return this.vorname;
+      return this.vorname;
     }
     // Sets the nachname of the PersonBO */
     setNachname(nachname) {
-    this.nachname = nachname;
+      this.nachname = nachname;
     }
     /** gets the nachname of the PersonBO */
     getNachname() {
-    return this.nachname;
+      return this.nachname;
     }
     // Sets the email of the PersonBO */
     setEmail(email) {
-    this.email = email;
+      this.email = email;
     }
     /** gets the email of the PersonBO */
     getEmail() {
-    return this.email;
+      return this.email;
     }
     // Sets the benutzername of the PersonBO */
     setBenutzername(benutzername) {
-    this.benutzername = benutzername;
+      this.benutzername = benutzername;
     }
     /** gets the benutzername of the PersonBO */
     getBenutzername() {
-    return this.benutzername;
+      return this.benutzername;
     }
-    // Sets the role of the PersonBO */
-    setRole(role) {
-      this.role = role;
-    }
-    /** gets the role of the PersonBO */
-    getRole() {
-      return this.role;
-    }
-
 
     /**
    * Returns an Array of  PersonBO from a given JSON structure
    */
     static fromJSON(person) {
         let result = [];
-    
+
         if (Array.isArray(person)) {
             person.forEach((c) => {
             Object.setPrototypeOf(c, PersonBO.prototype);
@@ -79,7 +68,7 @@ export default class PersonBO extends BusinessObject {
           Object.setPrototypeOf(c, PersonBO.prototype);
           result.push(c);
         }
-    
+
         return result;
       }
 }
