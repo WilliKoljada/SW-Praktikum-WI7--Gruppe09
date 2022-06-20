a4172cc8ba50fd68c982543265e677c853ba921e
Aniqa301 <103525488+Aniqa301@users.noreply.github.com>
2022-06-20 14:33:32 +0200

    Zeiten Delete Dialag angelegt

---
+++ frontend/src/components/dialogs/ZeitenDeleteDialog.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles, Button, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import ZeiterfassungAPI from "../../api/ZeiterfassungAPI";
+import ContextErrorMessage from "./ContextErrorMessage";
+import LoadingProgress from "./LoadingProgress";
+
+/**
+ * Shows a modal delete/cancle dialog, which asks for deleting a projekt. The ProjektBO to be deleted must be given in prop projekt.
+ * In dependency of the user interaction (delete/cancel) the respective backendcall is made. After that, the function of the onClose prop
+ * is called with the deleted ProjektBO object as parameter. When the dialog is canceled, onClose is called with null.
+ *
+ * @see See Material-UIs [Dialog](https://material-ui.com/components/dialogs)
+ */
+class ZeitenDeleteDialog extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Init the state
+    this.state = {
+      deletingInProgress: false,
+      deletingError: null
+    };
+  }
+
+  /** Delete the zeit */
+  deleteZeit = () => {
+    ZeiterfassungAPI.getAPI().deleteZeitIntervall(this.props.zeit.getID()).then(zeit => {
+      this.setState({
+        deletingInProgress: false,              // disable loading indicator
+        deletingError: null                     // no error message
+      });
+      this.props.onClose(this.props.zeit);  // call the parent with the deleted zeitintervall
+    }).catch(e =>
+      this.setState({
+        deletingInProgress: false,              // disable loading indicator
+        deletingError: e                        // show error message
+      })
+    );
+
+    // set loading to true
+    this.setState({
+      deletingInProgress: true,                 // show loading indicator
+      deletingError: null                       // disable error message
+    });
+  }
+
+  /** Handles the close / cancel button click event */
+  handleClose = () => {
+    // console.log(event);
+    this.props.onClose(null);
+  }
+
+  /** Renders the component */
+  render() {
+    const { classes, zeit, show } = this.props;
+    const { deletingInProgress, deletingError } = this.state;
+
+    return (
+      show ?
+        <Dialog open={show} onClose={this.handleClose}>
+          <DialogTitle id="delete-dialog-title">Delete Zeitintervall
+            <IconButton className={classes.closeButton} onClick={this.handleClose}>
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Really delete Zeitintervall "(ID: {zeit.getID()})?
+            </DialogContentText>
+            <LoadingProgress show={deletingInProgress} />
+            <ContextErrorMessage error={deletingError} contextErrorMsg={`The zeitintervall (ID: ${zeit.getID()}) could not be deleted.`}
+              onReload={this.deleteZeit} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="secondary">
+              Cancel
+            </Button>
+            <Button variant="contained" onClick={this.deleteZeit} color="primary">
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+        : null
+    );
+  }
+}
+
+/** Component specific styles */
+const styles = theme => ({
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  }
+});
+
+export default ZeitenDeleteDialog;
