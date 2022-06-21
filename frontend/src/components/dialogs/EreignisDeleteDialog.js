f87486c78c0f00b7164a82f368453bee8e99981f
Kaswara Al Mustafa <ka041@hdm-stuttgart.de>
2022-06-22 01:47:26 +0200

    EreignisDeleteDialog.js erstellt

---
+++ frontend/src/components/dialogs/EreignisDeleteDialog.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ZeiterfassungAPI from "../../api/ZeiterfassungAPI";
+import ContextErrorMessage from "./ContextErrorMessage";
+import LoadingProgress from "./LoadingProgress";
+
+
+
+
+
+
+/**
+ * Shows a modal delete/cancle dialog, which asks for deleting a ereignis. The EreignisBO to be deleted must be given in prop ereignis.
+ * In dependency of the user interaction (delete/cancel) the respective backendcall is made. After that, the function of the onClose prop
+ * is called with the deleted EreignisBO object as parameter. When the dialog is canceled, onClose is called with null.
+ *
+ * @see See Material-UIs [Dialog](https://material-ui.com/components/dialogs)
+ */
+class EreignisDeleteDialog extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Init the state
+    this.state = {
+      deletingInProgress: false,
+      deletingError: null
+    };
+  }
+
+  /** Delete the ereignis */
+  deleteEreignis = () => {
+    ZeiterfassungAPI.getAPI().deleteEreignis(this.props.ereignis.getID()).then(ereignis => {
+      this.setState({
+        deletingInProgress: false,              // disable loading indicator
+        deletingError: null                     // no error message
+      });
+      this.props.onClose(this.props.ereignis);  // call the parent with the deleted ereignis
+    }).catch(e =>
+      this.setState({
+        deletingInProgress: false,              // disable loading indicator
+        deletingError: e                        // show error message
+      })
+    );
+
