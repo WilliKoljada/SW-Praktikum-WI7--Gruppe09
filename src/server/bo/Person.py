80c718f8c4cb27581dd2956ca304c04b8c12d025
Ayhan <ayhancoemert@gmail.com>
2022-06-21 21:54:07 +0200

    Ergänzungen an Person und Projekt BO vorgenommen

--- src/server/bo/Person.py
+++ src/server/bo/Person.py
@@ -3,14 +3,14 @@ from server.bo import BusinessObject as bo
 
 
 class Person (bo.BusinessObject):
-
     def __init__(self):
         super().__init__() # creation_date und id wird nicht mehr im Bo reingeschrieben, da es vererbt wird.
-        self._vorname = ''
-        self._nachname = ''
-        self._email = ''
-        self._benutzername = ''
-        self._google_id = ''
+        self._vorname = ""
+        self._nachname = ""
+        self._email = ""
+        self._benutzername = ""
+        self._google_id = ""
+        self._role = ""
 
     def get_vorname (self):
         "Auslesen des vornamens"
@@ -44,22 +44,31 @@ class Person (bo.BusinessObject):
         "Akzeptieren der benutzername"
         self._benutzername = benutzername
 
-    def get_google_id (self):
+    def get_google_id(self):
         return self._google_id
 
-    def set_google_id (self, google_id):
+    def set_google_id(self, google_id):
         self._google_id = google_id
+
+    def get_role(self):
+        return self._role
+
+    def set_role(self, role):
+        self._role = role
 
 
     # Person aus dictionary erstellen
     @staticmethod
     def from_dict(dictionary=dict()):
+        print(dictionary)
         obj = Person()
         obj.set_id(dictionary["id"])
+        obj.set_creation_date(dictionary["creation_date"])
         obj.set_vorname(dictionary["vorname"])
         obj.set_nachname(dictionary["nachname"])
         obj.set_email(dictionary["email"])
         obj.set_benutzername(dictionary["benutzername"])
         obj.set_google_id(dictionary["google_id"])
+        obj.set_role(dictionary["role"])
 
         return obj
