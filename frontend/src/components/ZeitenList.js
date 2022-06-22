e36313ada2c0ce1f26a91593ca9656c18d6ea239
BenjaminTok <bt033@hdm-stuttgart.de>
2022-06-22 15:33:09 +0200

    in Zeitenlist.js class Zeitenlist erstellt

--- frontend/src/components/ZeitenList.js
+++ frontend/src/components/ZeitenList.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { withStyles, Button, Grid } from "@material-ui/core";
+import { withStyles, Button, TextField, InputAdornment, IconButton, Grid, Typography } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
-// import ClearIcon from "@material-ui/icons/Clear"
+import ClearIcon from "@material-ui/icons/Clear"
 import { withRouter } from "react-router-dom";
 import ZeiterfassungAPI from "../api/ZeiterfassungAPI";
 import ContextErrorMessage from "./dialogs/ContextErrorMessage";
@@ -11,3 +11,23 @@ import ZeitenForm from "./dialogs/ZeitenForm";
 import ZeitenListEntry from "./entries/ZeitenListEntry";
 
 class ZeitenList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // console.log(props);
+    let expandedID = null;
+
+    if(this.props.location.expandZeiten){
+      expandedID = this.props.location.expandZeiten.getID();
+    }
+
+    // Init the state
+    this.state = {
+      zeiten: [],
+      error: null,
+      loadingInProgress: false,
+      expandedZeitenID: expandedID,
+      showZeitenForm: false
+    };
+  }
