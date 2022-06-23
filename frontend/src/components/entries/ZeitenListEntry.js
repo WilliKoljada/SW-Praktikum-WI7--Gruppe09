747ca9d39532fd1f350839f7bbed1cb9e2d9718b
Kaswara Al Mustafa <ka041@hdm-stuttgart.de>
2022-06-23 23:32:55 +0200

    Ergänzungen vorgenommen

--- frontend/src/components/entries/ZeitenListEntry.js
+++ frontend/src/components/entries/ZeitenListEntry.js
@@ -64,3 +64,71 @@ class ZeitenListEntry extends Component {
     }
   }
 
+ /** Handles the onClick event of the delete zeit button */
+  deleteZeitenButtonClicked = (event) => {
+    event.stopPropagation();
+    this.setState({
+      showZeitenDeleteDialog: true
+    });
+  }
+
+  /** Handles the onClose event of the ZeitenDeleteDialog */
+  deleteZeitenDialogClosed = (zeit) => {
+    // if zeit is not null, delete it
+    if(zeit) {
+      this.props.onZeitenDeleted(zeit);
+    };
+
+    // Don´t show the dialog
+    this.setState({
+      showZeitenDeleteDialog: false
+    });
+  }
+
+  /** Renders the component */
+  render() {
+    const { classes, expandedState } = this.props;
+    // Use the szeit
+    const { zeit, showZeitenForm, showZeitenDeleteDialog } = this.state;
+
+    // console.log(this.state);
+    return (
+      <div>
+        <Accordion defaultExpanded={false} expanded={expandedState} onChange={this.expansionPanelStateChanged}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id={`zeit${zeit.getID()}zeitpanel-header`}
+          >
+            <Grid container spacing={1} justify="flex-start" alignItems="center">
+              <Grid item>
+                <Typography variant="body1" className={classes.heading}>
+                    {zeit.getID()},
+                </Typography>
+              </Grid>
+              <Grid item>
+                <ButtonGroup variant="text" size="small">
+                  <Button color="primary" onClick={this.ediZeitentButtonClicked}>
+                    edit
+                  </Button>
+                  <Button color="secondary" onClick={this.deleteZeitenButtonClicked}>
+                    delete
+                  </Button>
+                </ButtonGroup>
+              </Grid>
+              <Grid item xs />
+              <Grid item>
+                <Typography variant="body2" color={"textSecondary"}>List of Zeitintervall</Typography>
+              </Grid>
+            </Grid>
+          </AccordionSummary>
+          <AccordionDetails>
+            <ZeitenList show={expandedState} zeit={zeit} />
+          </AccordionDetails>
+        </Accordion>
+        <ZeitenForm show={showZeitenForm} zeit={zeit} onClose={this.zeitFormClosed} />
+        <ZeitenDeleteDialog show={showZeitenDeleteDialog} zeit={zeit} onClose={this.deleteZeitenDialogClosed} />
+      </div>
+    );
+  }
+}
+
