ec5b91f182b6d6f3128d2d38ab366874a41f0670
BenjaminTok <bt033@hdm-stuttgart.de>
2022-06-25 16:03:21 +0200

    in ZeitintervallBO.js Verbesserungen vorgenommen

--- frontend/src/api/ZeitintervallBO.js
+++ frontend/src/api/ZeitintervallBO.js
@@ -1,64 +1,54 @@
-import BusinessObject from './BusinessObject';
+import BusinessObject from "./BusinessObject";
 
 
 export default class ZeitintervallBO extends BusinessObject {
-  constructor(datum, start, end, aktivitaetID, userID){
+  constructor(datum, startzeit, endzeit, aktivitaetID, personID){
     super();
     this.datum = datum;
-    this.start = start;
-    this.end = end;
+    this.startzeit = startzeit;
+    this.endzeit = endzeit;
     this.aktivitaetID = aktivitaetID;
-    this.userID = userID;
+    this.personID = personID;
   }
-
-  // Sets the datum of the ZeitintervallBO */
+  /** sets the datum of the ZeitintervallBO */
   setDatum(datum) {
     this.datum = datum;
   }
-
   /** gets the datum of the ZeitintervallBO */
   getDatum() {
     return this.datum;
   }
-
-  // Sets the start of the ZeitintervallBO */
-  setStart(start) {
-    this.start = start;
+  /** sets the startzeit of the ZeitintervallBO */
+  setStartzeit(startzeit) {
+    this.startzeit = startzeit;
   }
-
-  /** gets the start of the ZeitintervallBO */
-  getStart() {
-    return this.start;
+  /** gets the startzeit of the ZeitintervallBO */
+  getStartzeit() {
+    return this.startzeit;
   }
-
-  // Sets the end of the ZeitintervallBO */
-  setEnd(end) {
-    this.end = end;
+  /** sets the endzeit of the ZeitintervallBO */
+  setEndzeit(endzeit) {
+    this.endzeit = endzeit;
   }
-
-  /** gets the end of the ZeitintervallBO */
-  getEnd() {
-    return this.end;
+  /** gets the endzeit of the ZeitintervallBO */
+  getEndzeit() {
+    return this.endzeit;
   }
-
+  /** sets the personID of the ZeitintervallBO */
+  setPersonID(personID) {
+    this.personID = personID;
+  }
+  /** gets the personID of the EreignisBO */
+  getPersonID() {
+    return this.personID;
+  }
   // Sets the aktivitaetID of the ZeitintervallBO */
   setAktivitaetID(aktivitaetID) {
     this.aktivitaetID = aktivitaetID;
   }
-
   /** gets the aktivitaetID of the ZeitintervallBO */
   getAktivitaetID() {
     return this.aktivitaetID;
-  }
-
-  // Sets the userID of the ZeitintervallBO */
-  setUserID(userID) {
-    this.userID = userID;
-  }
-
-  /** gets the userID of the ZeitintervallBO */
-  getUserID() {
-    return this.userID;
   }
 
   /**
