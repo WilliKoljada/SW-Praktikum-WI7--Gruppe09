03af4b0cd621ddda85b7ea6461b9205670335598
BenjaminTok <bt033@hdm-stuttgart.de>
2022-06-25 15:53:09 +0200

    in ZeitintervallBO.js Verbesserungen vorgenommen

--- frontend/src/api/ZeitintervallBO.js
+++ frontend/src/api/ZeitintervallBO.js
@@ -1,40 +1,72 @@
-import BusinessObject from './BusinessObject';
+import BusinessObject from "./BusinessObject";
 
 
 export default class ZeitintervallBO extends BusinessObject {
+  constructor(datum, startzeit, endzeit, aktivitaetID, personID){
+    super();
+    this.datum = datum;
+    this.startzeit = startzeit;
+    this.endzeit = endzeit;
+    this.aktivitaetID = aktivitaetID;
+    this.personID = personID;
+  }
+  /** sets the datum of the ZeitintervallBO */
+  setDatum(datum) {
+    this.datum = datum;
+  }
+  /** gets the datum of the ZeitintervallBO */
+  getDatum() {
+    return this.datum;
+  }
+  /** sets the startzeit of the ZeitintervallBO */
+  setStartzeit(startzeit) {
+    this.startzeit = startzeit;
+  }
+  /** gets the startzeit of the ZeitintervallBO */
+  getStartzeit() {
+    return this.startzeit;
+  }
+  /** sets the endzeit of the ZeitintervallBO */
+  setEndzeit(endzeit) {
+    this.endzeit = endzeit;
+  }
+  /** gets the endzeit of the ZeitintervallBO */
+  getEndzeit() {
+    return this.endzeit;
+  }
+  /** sets the personID of the ZeitintervallBO */
+  setPersonID(personID) {
+    this.personID = personID;
+  }
+  /** gets the personID of the EreignisBO */
+  getPersonID() {
+    return this.personID;
+  }
+  // Sets the aktivitaetID of the ZeitintervallBO */
+  setAktivitaetID(aktivitaetID) {
+    this.aktivitaetID = aktivitaetID;
+  }
+  /** gets the aktivitaetID of the ZeitintervallBO */
+  getAktivitaetID() {
+    return this.aktivitaetID;
+  }
 
-    constructor(projektlaufzeit){
-        super();
-        this.projektlaufzeit = projektlaufzeit;
-    }
-   
-    // Sets the projektlaufzeit of the ZeitintervallBO */
-    setProjektlaufzeit(projektlaufzeit) {
-    this.projektlaufzeit = projektlaufzeit;
-    }
-    /** gets the projektlaufzeit of the ZeitintervallBO */
-    getProjektlaufzeit() {
-    return this.projektlaufzeit;
-    }
-    
-    /**
+  /**
    * Returns an Array of  ZeitintervallBO from a given JSON structure
    */
-    static fromJSON(zeit_inter) {
-        let result = [];
-    
-        if (Array.isArray(zeit_inter)) {
-          zeit_inter.forEach((c) => {
-            Object.setPrototypeOf(c, ZeitintervallBO.prototype);
-            result.push(c);
-          })
-        } else {
-          // Es handelt sich offenbar um ein singuläres Objekt
-          let c = zeit_inter;
-          Object.setPrototypeOf(c, ZeitintervallBO.prototype);
-          result.push(c);
-        }
-    
-        return result;
-      }
+  static fromJSON(zeit_inter) {
+    let result = [];
+    if (Array.isArray(zeit_inter)) {
+      zeit_inter.forEach((c) => {
+        Object.setPrototypeOf(c, ZeitintervallBO.prototype);
+        result.push(c);
+      })
+    }else{
+      // Es handelt sich offenbar um ein singuläres Objekt
+      let c = zeit_inter;
+      Object.setPrototypeOf(c, ZeitintervallBO.prototype);
+        result.push(c);
+    }
+    return result;
+  }
 }
