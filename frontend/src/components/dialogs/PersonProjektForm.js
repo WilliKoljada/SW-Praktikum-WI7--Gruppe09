395ac770f97d7c1e19edf6284102adba6655836a
Kaswara Al Mustafa <ka041@hdm-stuttgart.de>
2022-07-02 19:26:17 +0200

    Ergänzungen vorgenommen (PropTypes Hinzugefügt)

--- frontend/src/components/dialogs/PersonProjektForm.js
+++ frontend/src/components/dialogs/PersonProjektForm.js
@@ -176,3 +176,36 @@ class PersonProjektForm extends Component {
   }
 }
 
+/** Component specific styles */
+const styles = theme => ({
+  root: {
+    width: "100%",
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+/** PropTypes */
+PersonProjektForm.propTypes = {
+  /** @ignore */
+  classes: PropTypes.object.isRequired,
+  /** The AktivitaetBO to be edited */
+  person: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  /** If true, the form is rendered */
+  show: PropTypes.bool.isRequired,
+  /**
+   * Handler function which is called, when the dialog is closed.
+   * Sends the edited or created AktivitaetBO as parameter or null, if cancel was pressed.
+   *
+   * Signature: onClose(AktivitaetBO aktivitaet);
+   */
+  onClose: PropTypes.func.isRequired,
+}
+
+export default withStyles(styles)(PersonProjektForm);
+
